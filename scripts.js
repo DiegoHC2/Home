5c745f2110bc9e5c98f914c9fd7aa9314374c383
Diego <diegonrriqueaz123@hotmail.com>
2023-04-04 18:53:17 -0300

    Design 1.0

--- scripts.js
+++ scripts.js
@@ -1,12 +1,8 @@
-let a = localStorage.getItem('users')
-let b = JSON.parse(a)
-
-let c = b.length
-
-for(i = 0; i<c; i++){
-
-    console.log(b[i])
-
+window.addEventListener("scroll", function(){
+    var menu = document.querySelector("nav");
+    var logo_control = document.getElementById("Logo");
     
-
-}
+    
+    menu.classList.toggle("ativo", window.scrollY > 30);
+    logo_control.classList.toggle("sticky", window.scrollY > 30) 
+})
